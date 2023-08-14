17600823b69723a12229a483d9207db834c2e102
Maitreya Gangurde <94222925+Maitreya444@users.noreply.github.com>
2023-08-14 22:46:52 +0530

    Add files via upload

---
+++ HA1_3.java
@@ -0,0 +1,12 @@
+public class HA1_3 
+{
+    public static void main(String[] args) 
+    {
+        int iCnt = 0;
+        
+        for(iCnt = 5; iCnt >=1 ; iCnt --)
+        {
+            System.out.println(iCnt);
+        }
+    }
+}
---
+++ HA1_3.ts
@@ -0,0 +1,10 @@
+function main()
+{
+    let iCnt : number = 0;
+    
+    for(iCnt =5; iCnt >=1; iCnt--)
+    {
+        console.log(iCnt)
+    }
+}
+main()
