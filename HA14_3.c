e706501cc9312e0a6eee468c4960e53d9afb4ea7
Maitreya Gangurde <94222925+Maitreya444@users.noreply.github.com>
2023-12-31 21:19:08 +0530

    Add files via upload

---
+++ HA14_3.c
@@ -0,0 +1,24 @@
+//Input : 5
+//Output : 5 * 4 * 3 * 2 * 1 *
+
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+    for(iCnt = iNo; iCnt >= 1; iCnt--)
+    {
+        printf(" %d*\t", iCnt);
+    }   
+}
+
+int main()
+{
+    int iValue = 0;
+    printf("Enter a Number \n");
+    scanf("%d", &iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
---
+++ HA14_5.c
@@ -0,0 +1,24 @@
+//Input : 8
+//Output : 2    4   6   8   10  12  14  16  
+
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+    for(iCnt = 2; iCnt <= iNo; iCnt++)
+    {
+        printf(" %d\t", iCnt*2);
+    }   
+}
+
+int main()
+{
+    int iValue = 0;
+    printf("Enter a Number \n");
+    scanf("%d", &iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
